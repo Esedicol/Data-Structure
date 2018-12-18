6fc7914583a863c9499305d1c6ac0d7382b19d9e
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2018-12-18 13:11:44 +0000

    added Check Balance class which will check for balance open and close ( {}, () )

---
+++ Calculator/src/CheckBalance.java
@@ -0,0 +1,23 @@
+/**
+ * @author Emmanuel Sedicol
+ * 
+ * This class is for checking wether we a balance open and close (parenthesis and brackets).
+*/
+
+import java.util.Stack;
+
+
+public class CheckBalance {
+	
+	Stack<String> s  = new Stack<>();
+	String nextChar;
+	
+	public static void main(String args[])
+	{
+		CheckBalance cb = new CheckBalance();
+		
+
+	}
+	
+
+}
